090a97cd32bc1a15eff9bf42a1d61a59e44a200d
Kim <xzsa18@naver.com>
2022-10-10 21:19:18 +0900

    버튼 클릭시 Edit에 써놨던 문구를 Textview에 출력

--- app/src/main/java/com/example/uikotlinpractice/MainActivity.kt
+++ app/src/main/java/com/example/uikotlinpractice/MainActivity.kt
@@ -2,12 +2,20 @@ package com.example.uikotlinpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//     확인 버튼이 눌릴시 이벤트
 
+        okBtn.setOnClickListener {
+
+            val inputContent = contentEdt.text.toString()
+
+            resultTxt.text = inputContent
+        }
     }
 }
